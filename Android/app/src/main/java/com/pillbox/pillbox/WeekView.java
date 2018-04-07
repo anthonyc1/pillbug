519358495c81fd70490947525c3da4288fd11928
Anthony Chan <anthonychan0123@gmail.com>
2018-04-07 18:54:18 -0400

    Improved design + implemented more frontend

--- Android/app/src/main/java/com/pillbox/pillbox/WeekView.java
+++ Android/app/src/main/java/com/pillbox/pillbox/WeekView.java
@@ -2,6 +2,10 @@ package com.pillbox.pillbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class WeekView extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class WeekView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_view);
+
+//        GridView gridview = (GridView) findViewById(R.id.gridview);
+//        gridview.setAdapter(new ImageAdapter(this));
+//
+//        gridview.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(WeekView.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
